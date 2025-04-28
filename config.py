1b66349344877d5768c1971d01e632518b4d1988
Jashan94600 <sandeepkunar.887@gmail.com>
2025-04-28 20:06:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "27463406"
+api_hash = "40229f3885b5bc03ba01a8fe78a9c073"
+bot_token = "7535478624:AAF6tUXkJ3Zzzqh3vJyOem-spYV-8Ukn5dI"
+auth_users = [683630491]
